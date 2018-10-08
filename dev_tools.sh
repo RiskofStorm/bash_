e1831dc51894452ac6d57da3592dd35fa53b7019
riskofstorm <voidnonenullnan@gmail.com>
2018-10-08 20:12:07 +0300

    add

--- dev_tools.sh
+++ dev_tools.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
-# comment that You don't want to install
 
-sudo apt-get install git
-sudo snap install --classic heroku
+# INSTALL GIT
+sudo apt-get install git -y
 
+# INSTALL HEROKU CLIENT
+sudo snap install --classic heroku -y
+
+
+# INSTALL GOOGLE DRIVE
+sudo add-apt-repository ppa:alessandro-strada/ppa
+sudo apt update
+sudo apt install google-drive-ocamlfuse -y
+
+cd $HOME
+mkdir ~/google-drive
+
+google-drive-ocamlfuse ~/google-drive/
+# unmount
+# fusermount -u ~/google-drive
+
+
+# INSTALL
--- jupyter_install.sh
+++ jupyter_install.sh
@@ -10,12 +10,29 @@ conda activate py27
 conda deactivate
 echo "Python 2 kernel -- DONE"
 
+# TODO: make it work. cli/target - where is it?
 # SCALA KERNEL INSTALL
-git clone https://github.com/alexarchambault/jupyter-scala.git
-sudo apt-get install sbt
-cd jupyter-scala
-sbt cli/packArchive
-echo "Scala kernel -- DONE"
+#git clone https://github.com/alexarchambault/jupyter-scala.git
+#echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+#sudo apt-get update
+#sudo apt-get install sbt -y
+#cd jupyter-scala
+#sbt cli/packArchive
+
+
+#git clone https://github.com/alexarchambault/jupyter-scala.git
+#cd jupyter-scala
+#sbt cli/packArchive
+#sbt publishM2
+## unpack cli/target/jupyter-scala_2.11.6-0.2.0-SNAPSHOT.zip
+#cd cli/target/jupyter-scala_2.11.6-0.2.0-SNAPSHOT/bin
+#./jupyter-scala
+
+
+#cd ..
+#rm -rf jupyter-scala
+#echo "Scala kernel -- DONE"
 
 # R KERNEL ADD !!!  must be already installed R  !!! official site  https://www.r-project.org/
 sudo apt-get install libcurl4-openssl-dev libssl-dev
