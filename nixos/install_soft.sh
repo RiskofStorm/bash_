f7c028a5f88506da5d43f265e0c4c10647fa0d65
Daniel Abramov <voidnonenullnan@gmail.com>
2018-12-30 15:53:58 +0300

    NixOS configuration, packages

---
+++ nixos/install_soft.sh
@@ -0,0 +1,7 @@
+nix-env -iA nixos.jetbrains.pycharm-community
+nix-env -iA nixos.jetbrains.idea-community
+pkgs/development/python-modules/conda/default.nix
+nix-env -iA nixos.rstudio
+nix-env -iA nixos.jre10
+pkgs/development/compilers/openjdk/10.nix
+nix-env -iA nixos.tdesktop
